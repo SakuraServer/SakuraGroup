a8798b5a9ccae6ad4780500ccf05100543c6cec4
syamn <admin@sakura-server.net>
2012-10-18 16:43:20 +0900

    Use queue while running leave command.

--- src/main/java/syam/sakuragroup/command/LeaveCommand.java
+++ src/main/java/syam/sakuragroup/command/LeaveCommand.java
@@ -7,9 +7,11 @@ package syam.sakuragroup.command;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
 import syam.sakuragroup.Group;
 import syam.sakuragroup.SakuraGroup;
+import syam.sakuragroup.command.queue.Queueable;
 import syam.sakuragroup.database.Database;
 import syam.sakuragroup.exception.CommandException;
 import syam.sakuragroup.manager.PEXManager;
@@ -21,7 +23,7 @@ import syam.sakuragroup.util.Util;
  * LeaveCommand (LeaveCommand.java)
  * @author syam(syamn)
  */
-public class LeaveCommand extends BaseCommand {
+public class LeaveCommand extends BaseCommand implements Queueable{
 	public LeaveCommand(){
 		bePlayer = true;
 		name = "leave";
@@ -31,50 +33,60 @@ public class LeaveCommand extends BaseCommand {
 
 	@Override
 	public void execute() throws CommandException {
-		PEXManager mgr = plugin.getPEXmgr();
-
-		// 引数なし
-		if (args.size() == 0){
-			final String defGroup = plugin.getConfigs().getDefGroup();
-			String currentGroup = mgr.getCurrentGroup(player);
-			if (defGroup.equalsIgnoreCase(currentGroup)){
-				throw new CommandException("あなたは既にデフォルトグループメンバーです！");
-			}
-
-			// prepare update
-			final Long unixtime = Util.getCurrentUnixSec();
-			int status = 0;
-			int changed = 0;
-
-			// Get Database
-			Database db = SakuraGroup.getDatabases();
-			HashMap<Integer, ArrayList<String>> result =
-					db.read("SELECT `status`, `changed` FROM " + db.getTablePrefix() + "users WHERE `player_name` = ?", player.getName());
-			if (result.size() > 0){
-				// 既にDB登録済み チェック
-				ArrayList<String> record = result.get(1);
-
-				status = Integer.valueOf(record.get(0));
-				changed = Integer.valueOf(record.get(1));
-
-				// ステータスチェック
-				if (status != 0){
-					throw new CommandException("あなたはグループの変更を禁止されています！");
-				}
-			}
-
-			// Update
-			db.write("REPLACE INTO " + db.getTablePrefix() + "users (`player_name`, `group`, `status`, `changed`, `lastchange`) " +
-					"VALUES (?, ?, ?, ?, ?)", player.getName(), defGroup, status, changed, unixtime.intValue());
-
-			// Change group
-			mgr.changeGroup(player.getName(), defGroup, null);
-
-			// messaging
-			Actions.broadcastMessage(msgPrefix+ "&6" + player.getName() + "&aさんがデフォルトグループに戻りました！");
-		}else{
-			throw new CommandException("使い方を誤っています！");
+		String currentGroup = plugin.getPEXmgr().getCurrentGroup(player);
+		if (plugin.getConfigs().getDefGroup().equalsIgnoreCase(currentGroup)){
+			throw new CommandException("あなたは既にデフォルトグループメンバーです！");
 		}
+
+		plugin.getQueue().addQueue(sender, this, args, 15);
+		Actions.message(sender, "&dデフォルトグループに戻ろうとしています！");
+		Actions.message(sender, "&dこの操作は取り消しすることができません。本当に続行しますか？");
+		Actions.message(sender, "&d続行するには &a/group confirm &dコマンドを入力してください！");
+		Actions.message(sender, "&a/group confirm &dコマンドは15秒間のみ有効です。");
+	}
+
+	@Override
+	public void executeQueue(List<String> qArgs){
+		PEXManager mgr = plugin.getPEXmgr();
+		final String defGroup = plugin.getConfigs().getDefGroup();
+
+		if (defGroup.equalsIgnoreCase(plugin.getPEXmgr().getCurrentGroup(player))){
+			Actions.message(player, "&cあなたは既にデフォルトグループメンバーです！");
+			return;
+		}
+
+		// prepare update
+		final Long unixtime = Util.getCurrentUnixSec();
+		int status = 0;
+		int changed = 0;
+
+		// Get Database
+		Database db = SakuraGroup.getDatabases();
+		HashMap<Integer, ArrayList<String>> result =
+				db.read("SELECT `status`, `changed` FROM " + db.getTablePrefix() + "users WHERE `player_name` = ?", player.getName());
+		if (result.size() > 0){
+			// 既にDB登録済み チェック
+			ArrayList<String> record = result.get(1);
+
+			status = Integer.valueOf(record.get(0));
+			changed = Integer.valueOf(record.get(1));
+
+			// ステータスチェック
+			if (status != 0){
+				Actions.message(player, "&cあなたはグループの変更を禁止されています！");
+				return;
+			}
+		}
+
+		// Update
+		db.write("REPLACE INTO " + db.getTablePrefix() + "users (`player_name`, `group`, `status`, `changed`, `lastchange`) " +
+				"VALUES (?, ?, ?, ?, ?)", player.getName(), defGroup, status, changed, unixtime.intValue());
+
+		// Change group
+		mgr.changeGroup(player.getName(), defGroup, null);
+
+		// messaging
+		Actions.broadcastMessage(msgPrefix+ "&6" + player.getName() + "&aさんがデフォルトグループに戻りました！");
 	}
 
 	@Override
